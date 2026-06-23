6a8324403c44c282552219ffa510ca2fa256af98
ligus <75608937+ligusx@users.noreply.github.com>
2026-06-23 14:12:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
 
 # Copy custom local packages into OpenWrt tree so they are available during build
 if [ -d "$GITHUB_WORKSPACE/package/luci-compat-keep" ]; then
